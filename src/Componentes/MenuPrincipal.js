41d3c3fb57bc8ed851c02fad4da65e1585aa406d
Malena de Juan <malena.dj4@gmail.com>
2021-04-16 12:32:56 +0200

    cambiada lista nav por Navs

--- src/Componentes/MenuPrincipal.js
+++ src/Componentes/MenuPrincipal.js
@@ -1,15 +1,17 @@
-import { Col, Button, Row } from "react-bootstrap";
+import {
+  Col, Button, Nav
+} from "react-bootstrap";
 
 const MenuPrincipal = () => (
-  <Col xs={8} as="nav" className="navMenu">
-    <Row as="ul" className="list-unstyled navLista">
-      <Col as="li" className="navLista"><span className="navItem">Nueva Incidencia</span></Col>
-      <Col as="li" className="navLista"><span className="navItem">Lista Incidencias</span></Col>
-      <Col as="li" className="navLista"><span className="navItem"> Mis incidencias</span></Col>
-      <Col as="li" className="navLista"><span className="navItem">Mi cuenta</span></Col>
-      <Col as="li" className="navLista"><span className="navItem">Contacto</span></Col>
-      <Col as="li" className="navLista"><Button variant="dark" className="navItem registro">Registro</Button></Col>
-    </Row>
+  <Col xs={8} className="navMenu">
+    <Nav className="navLista">
+      <Nav.Item className="navLista"><Nav.Link className="navItem">Nueva Incidencia</Nav.Link></Nav.Item>
+      <Nav.Item className="navLista"><Nav.Link className="navItem">Lista Incidencias</Nav.Link></Nav.Item>
+      <Nav.Item className="navLista"><Nav.Link className="navItem"> Mis incidencias</Nav.Link></Nav.Item>
+      <Nav.Item className="navLista"><Nav.Link className="navItem">Mi cuenta</Nav.Link></Nav.Item>
+      <Nav.Item className="navLista"><Nav.Link className="navItem">Contacto</Nav.Link></Nav.Item>
+      <Nav.Item className="navLista"><Button variant="dark" className="navItem registro">Registro</Button></Nav.Item>
+    </Nav>
   </Col>
 );
 
