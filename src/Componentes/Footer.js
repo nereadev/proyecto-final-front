dba54c51816af33eb0b824cc469117150d1a5b0e
Malena de Juan <malena.dj4@gmail.com>
2021-04-16 14:01:56 +0200

    footer responsive y alto cabecera modificado

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -4,12 +4,12 @@ import logo from "../img/logo.png";
 
 const Footer = () => (
   <Row as="footer" className="pie">
+    <MenuFooter />
+    <Col sm={12} className="textoFooter">
+      <span className="copyright">Aquí irá información del copyright</span>
+    </Col>
     <Col as="span" className="espacioLogo">
       <img src={logo} alt="Logotipo de SomDev" className="logoWeb" />
-    </Col>
-    <MenuFooter />
-    <Col className="textoFooter">
-      <span className="copyright">Aquí irá información del copyright</span>
     </Col>
   </Row>
 );
