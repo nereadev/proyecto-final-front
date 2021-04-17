55f88882f229f9e85b96879689578d9c1dd625d4
Nerea García <nereagarcia.c@gmail.com>
2021-04-17 10:40:15 +0200

    anyadir Bootstrap FormularioPagina

--- src/paginas/FormularioPagina.js
+++ src/paginas/FormularioPagina.js
@@ -1,56 +1,60 @@
-import { Form } from "react-bootstrap";
+import {
+  Button, Col, Form, Row
+} from "react-bootstrap";
 
 const FormularioPagina = () => (
   <>
-    <h2>Formulario Incidencia</h2>
-    <section>
-      <Form>
-        <Form.Label htmlFor="nombre-incidenica">
-          Nombre Incidencia:
-          <input id="nombre-incidenica" type="text" />
-        </Form.Label>
-        <Form.Label htmlFor="descripcion">
-          Descripción:
-          <input id="descripcion" type="text" />
-        </Form.Label>
-        <Form.Label htmlFor="fotografía">
-          Fotografía:
-          <input type="file" />
-        </Form.Label>
-        <Form.Label htmlFor="tipo">
-          Tipo:
-          <select id="tipo-incidencias">
-            <option value="medio-ambiente">Medio Ambiente</option>
-            <option value="civismo">civismo</option>
-            <option value="infraestructura">Infraestructura</option>
-            <option value="otros">
-              Otros:
-            </option>
-          </select>
-        </Form.Label>
-        <Form.Label htmlFor="localizacion">
-          Localización:
-          <select id="tipo-localizacion">
-            <option value="localizacion-manual">
-              Introducir datos
-              {/*        <label htmlFor="codigo-postal">
-                Código Postal:
-                <input type="text" />
-              </label>
-              <label htmlFor="direccion">
-                Dirección:
-                <input type="text" />
-              </label>
-              <label htmlFor="comentarios">
-                Comentarios:
-                <input type="text" />
-              </label> */}
-            </option>
-            <option value="localizacion-ubicacion">Usar Ubicación</option>
-          </select>
-        </Form.Label>
+    <Row as="h2">Formulario Incidencia</Row>
+    <Row as="section" className="formulario incidencia">
+      <Form as={Col} md={10}>
+        <Form.Group>
+          <Form.Label>Nombre Incidencia:</Form.Label>
+          <Form.Control
+            required
+            type="text"
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Tipo:</Form.Label>
+          <Form.Control as="Select" defaulValue="Elige...">
+            <option>Elige...</option>
+            <option>Medio Ambiente</option>
+            <option>Civismo</option>
+            <option>Infraestrucutra</option>
+            <option>Otros:</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Descripción:</Form.Label>
+          <Form.Control as="textarea" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Fotografía:</Form.Label>
+          <Form.File label="(Formato perimitido: jpng .png | Tamaño 3 Mb)" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Localización:</Form.Label>
+          <Form.Check type="checkbox" label="Introducir datos" />
+          <Form.Label>Código Postal:</Form.Label>
+          <Form.Control
+            required
+            type="text"
+          />
+          <Form.Label>Dirección</Form.Label>
+          <Form.Control
+            required
+            type="text"
+          />
+          <Form.Label>Comentario:</Form.Label>
+          <Form.Control
+            required
+            type="text"
+          />
+          <Form.Check type="checkbox" label="Usar ubicación" />
+        </Form.Group>
+        <Button className="boton-crear" type="submit" variant="info">Registrar</Button>
       </Form>
-    </section>
+    </Row>
   </>
 );
 
