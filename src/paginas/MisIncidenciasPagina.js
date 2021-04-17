94e6690001adb35aeedf08fb0e997f5d244ea8f2
Nerea García <nereagarcia.c@gmail.com>
2021-04-17 17:42:02 +0200

    crear Bootstrap MisIncidenciasPainga

--- src/paginas/MisIncidenciasPagina.js
+++ src/paginas/MisIncidenciasPagina.js
@@ -1,16 +1,28 @@
+import { Col, Row } from "react-bootstrap";
+
 const MisIncidenciasPagina = () => (
   <>
-    <h2>Mis Incidenicas</h2>
-    <section>
-      <ul>
-        <li>
-          <p>Nombre incidencia</p>
-          <p>Tipo incidencia</p>
-          <p>Descripción Incidencia</p>
-          <p>Favorito</p>
-        </li>
-      </ul>
-    </section>
+    <Row as="h2">Mis Incidenicas</Row>
+    <Row as="section" className="lista-incidencias">
+      <Col as="ul">
+        <Row className="titulo-lista" as="li">
+          <Col><i className="fas fa-list-ul" /></Col>
+          <Col>Nombre</Col>
+          <Col>Tipo</Col>
+          <Col>Descripción</Col>
+          <Col><i className="fas fa-star" /></Col>
+          <Col>+info</Col>
+        </Row>
+        <Row as="li">
+          <Col>1</Col>
+          <Col>Árbol caído</Col>
+          <Col>Medio Ambiente</Col>
+          <Col>Hay un árbol caído</Col>
+          <Col>7</Col>
+          <Col><i className="fas fa-plus" /></Col>
+        </Row>
+      </Col>
+    </Row>
   </>
 );
 
