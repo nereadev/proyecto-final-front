55445a7f622f50663caa064b90f02a3ac721c932
Malena de Juan <malena.dj4@gmail.com>
2021-04-18 18:39:25 +0200

    links del menu footer (no funcionan)

--- src/Componentes/MenuPrincipal.js
+++ src/Componentes/MenuPrincipal.js
@@ -6,11 +6,11 @@ const MenuPrincipal = () => (
   <Col xs={10}>
     <Nav className="navMenu">
       <Nav.Item className="navLista"><Nav.Link href="/incidencia" className="navItem">Nueva Incidencia</Nav.Link></Nav.Item>
-      <Nav.Item className="navLista"><Nav.Link href="inicio" className="navItem">Mapa / Lista incidencias</Nav.Link></Nav.Item>
-      <Nav.Item className="navLista"><Nav.Link href="mis-incidencias" className="navItem"> Mis incidencias</Nav.Link></Nav.Item>
-      <Nav.Item className="navLista"><Nav.Link href="como-funciona" className="navItem"> Cómo funciona?</Nav.Link></Nav.Item>
-      <Nav.Item className="navLista"><Nav.Link href="mi-cuenta" className="navItem">Configuración</Nav.Link></Nav.Item>
-      <Nav.Item className="navLista"><Nav.Link href="contacto" className="navItem">Contacto</Nav.Link></Nav.Item>
+      <Nav.Item className="navLista"><Nav.Link href="/inicio" className="navItem">Mapa / Lista incidencias</Nav.Link></Nav.Item>
+      <Nav.Item className="navLista"><Nav.Link href="/mis-incidencias" className="navItem"> Mis incidencias</Nav.Link></Nav.Item>
+      <Nav.Item className="navLista"><Nav.Link href="/como-funciona" className="navItem"> Cómo funciona?</Nav.Link></Nav.Item>
+      <Nav.Item className="navLista"><Nav.Link href="/mi-cuenta" className="navItem">Configuración</Nav.Link></Nav.Item>
+      <Nav.Item className="navLista"><Nav.Link href="/contacto" className="navItem">Contacto</Nav.Link></Nav.Item>
       <Nav.Item className="navLista"><Button variant="dark" href="registro" className="navItem registroBoton">Registro</Button></Nav.Item>
     </Nav>
   </Col>
