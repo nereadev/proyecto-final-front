30c2f26195ca453ec87b01d364e521eb1317cfbf
Nerea García <nereagarcia.c@gmail.com>
2021-04-19 12:51:52 +0200

    link mis incidencias a detalle incidencia

--- src/paginas/MisIncidenciasPagina.js
+++ src/paginas/MisIncidenciasPagina.js
@@ -1,31 +1,12 @@
 import { Col, Row } from "react-bootstrap";
 import Cabecera from "../componentes/Cabecera";
 import Footer from "../componentes/Footer";
+import ListaIncidencias from "../componentes/ListaIncidencias";
 
 const MisIncidenciasPagina = () => (
   <>
     <Cabecera />
-    <Row as="h2">Mis Incidenicas</Row>
-    <Row as="section" className="lista-incidencias">
-      <Col as="ul">
-        <Row className="titulo-lista" as="li">
-          <Col><i className="fas fa-list-ul" /></Col>
-          <Col>Nombre</Col>
-          <Col>Tipo</Col>
-          <Col>Descripción</Col>
-          <Col><i className="fas fa-star" /></Col>
-          <Col>+info</Col>
-        </Row>
-        <Row as="li">
-          <Col>1</Col>
-          <Col>Árbol caído</Col>
-          <Col>Medio Ambiente</Col>
-          <Col>Hay un árbol caído</Col>
-          <Col>7</Col>
-          <Col><i className="fas fa-plus" /></Col>
-        </Row>
-      </Col>
-    </Row>
+    <ListaIncidencias />
     <Footer />
   </>
 );
