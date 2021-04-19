7d7048d14c0c9365499915e3d9dcedbf0761e42a
Malena de Juan <malena.dj4@gmail.com>
2021-04-19 10:09:10 +0200

    link a inicio des de titulo

--- src/Componentes/Cabecera.js
+++ src/Componentes/Cabecera.js
@@ -4,7 +4,7 @@ import MenuPrincipal from "./MenuPrincipal";
 const Cabecera = () => (
   <Row as="header" className="cabecera">
     <Col xs={2} as="h2" className="titulo">
-      Ciutadà Verd
+      <a href="/inicio">Ciutadà Verd</a>
     </Col>
     <MenuPrincipal />
   </Row>
