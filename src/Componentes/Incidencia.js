d5ecc179e24b8f699517cc16643505c2c96da273
Malena de Juan <malena.dj4@gmail.com>
2021-04-19 10:53:40 +0200

    linkado boton incidencia a su futura pagina

--- src/Componentes/Incidencia.js
+++ src/Componentes/Incidencia.js
@@ -1,14 +1,17 @@
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
-const Incidencia = () => (
-  <Row className="incidencia-lista">
-    <Col>1</Col>
-    <Col>Árbol caído</Col>
-    <Col>Medio Ambiente</Col>
-    <Col>Hay un árbol caído</Col>
-    <Col>7</Col>
-    <Col><i className="fas fa-plus" /></Col>
-  </Row>
-);
+const Incidencia = () => {
+  const id = "1";
+  return (
+    <Row className="incidencia-lista">
+      <Col>1</Col>
+      <Col>Árbol caído</Col>
+      <Col>Medio Ambiente</Col>
+      <Col>Hay un árbol caído</Col>
+      <Col>7</Col>
+      <Col><Button variant="secondary" href={`/incidencia/${id}`} className="detalle"><i className="fas fa-plus" /></Button></Col>
+    </Row>
+  );
+};
 
 export default Incidencia;
