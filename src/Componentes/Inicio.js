23ea8c3fdc5e1f514c19f3a4df96bce53aaf3fe8
Malena de Juan <malena.dj4@gmail.com>
2021-04-19 09:49:06 +0200

    corregido el toggle inicio mapa o listado

--- src/Componentes/Inicio.js
+++ src/Componentes/Inicio.js
@@ -1,21 +1,18 @@
 import { useState } from "react";
 import {
-  ButtonGroup, Button, Row, Col
+  Button, Row, Col
 } from "react-bootstrap";
 import Listado from "./Listado";
 import Mapa from "./Mapa";
 
 const Inicio = () => {
   const [listado, setListado] = useState(false);
-  const toggleContenido = () => {
-    setListado(!listado);
-  };
 
   return (
     <Row as="section" className="contenidoInicio">
-      <Col sm={12} as="ButtonGroup" className="mapa-lista">
-        <Button variant="secondary" onClick={toggleContenido} className="botonMapaLista">Mapa</Button>
-        <Button variant="secondary" onClick={toggleContenido} className="botonMapaLista">Listado</Button>
+      <Col sm={12} className="mapa-lista">
+        <Button variant="secondary" onClick={() => setListado(false)} className="botonMapaLista">Mapa</Button>
+        <Button variant="secondary" onClick={() => setListado(true)} className="botonMapaLista">Listado</Button>
       </Col>
       { listado ? <Listado /> : <Mapa />}
     </Row>
