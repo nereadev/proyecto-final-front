4f6760e196960b85fce721df48eb368577d88a8b
Malena de Juan <malena.dj4@gmail.com>
2021-04-20 17:04:47 +0200

    añadidas cols y rows

--- src/paginas/ComoFunciona.js
+++ src/paginas/ComoFunciona.js
@@ -10,20 +10,19 @@ const ComoFunciona = () => (
       </Col>
       <Col as="section">
         <h3>¿Quieres registrar una incidencia?</h3>
-        <ul>
+        <Row as="ul" className="puntoFuncionamiento">
           <li>Primero comprueba si alguien ya ha reportado esa incidencia. ¿Cómo?</li>
           <ul>
             <li>Localiza la incidencia en el mapa</li>
             <li>Consulta el listado de incidencias utilizando filtros o el buscador para encontrar la incidencia</li>
           </ul>
-          <li>Si la incidencia ya está registrada puedes darle tu voto para mejorar su posicionamiento</li>
-
-          <li>Si todavía no está registrada ve al formulario de Nueva incidencia a través del menú de navegación</li>
+          <Col as="li">Si la incidencia ya está registrada puedes darle tu voto para mejorar su posicionamiento</Col>
+          <Col as="li">Si todavía no está registrada ve al formulario de Nueva incidencia a través del menú de navegación</Col>
           <li>Consulta Mis incidencias para hacer el seguimiento de las incidencias que hayas registrado</li>
           <li>
             Si detectas que una incidencia está duplicada puedes reportarlo en el detalle de la incidencia
           </li>
-        </ul>
+        </Row>
       </Col>
     </Row>
   </>
