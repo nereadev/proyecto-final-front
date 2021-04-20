ce48e90400a4242cf0ac1da5cdd0ebf9ee98c7ea
Malena de Juan <malena.dj4@gmail.com>
2021-04-20 13:45:56 +0200

    creado el contenido hmtl de la página

--- src/paginas/ComoFunciona.js
+++ src/paginas/ComoFunciona.js
@@ -1,9 +1,30 @@
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 const ComoFunciona = () => (
   <>
     <Row as="main">
-      <span>Aquí se explicará el funcionamiento de la App</span>
+      <Col sm={12} as="h2">Cómo funciona?</Col>
+      <Col as="section">
+        <h3>Infórmate sobre qué incidencias hay en tu zona</h3>
+        <h3>Regístrate para votar incidencias o regístrarlas tú mismo</h3>
+      </Col>
+      <Col as="section">
+        <h3>¿Quieres registrar una incidencia?</h3>
+        <ul>
+          <li>Primero comprueba si alguien ya ha reportado esa incidencia. ¿Cómo?</li>
+          <ul>
+            <li>Localiza la incidencia en el mapa</li>
+            <li>Consulta el listado de incidencias utilizando filtros o el buscador para encontrar la incidencia</li>
+          </ul>
+          <li>Si la incidencia ya está registrada puedes darle tu voto para mejorar su posicionamiento</li>
+
+          <li>Si todavía no está registrada ve al formulario de Nueva incidencia a través del menú de navegación</li>
+          <li>Consulta Mis incidencias para hacer el seguimiento de las incidencias que hayas registrado</li>
+          <li>
+            Si detectas que una incidencia está duplicada puedes reportarlo en el detalle de la incidencia
+          </li>
+        </ul>
+      </Col>
     </Row>
   </>
 );
