fe78db63ef3678e75eb59bad60c1b97181bc234c
Bernat Jorquera <bernatjv19@gmail.com>
2021-04-20 19:34:37 +0200

    Bug de merge solucionado

--- src/App.js
+++ src/App.js
@@ -14,45 +14,48 @@ import ComoFunciona from "./paginas/ComoFunciona";
 import NuevaIncidenciaPagina from "./paginas/NuevaIncidenciaPagina";
 import Cabecera from "./componentes/Cabecera";
 import Footer from "./componentes/Footer";
+import ContextoIncidenciasProvider from "./contextos/ContextoIncidenciasProvider";
 
 function App() {
   return (
     <Container fluid>
       <Cabecera />
-      <Router>
-        <Switch>
-          <Route path="/inicio" exact>
-            <InicioPagina />
-          </Route>
-          <Route path="/registro/:accion" exact>
-            <RegistroPagina />
-          </Route>
-          <Route path="/incidencia/:id" exact>
-            <IncidenciaPagina />
-          </Route>
-          <Route path="/nueva-incidencia" exact>
-            <NuevaIncidenciaPagina />
-          </Route>
-          <Route path="/mi-cuenta" exact>
-            <MiCuentaPagina />
-          </Route>
-          <Route path="/mis-incidencias" exact>
-            <MisIncidenciasPagina />
-          </Route>
-          <Route path="/contacto" exact>
-            <ContactoPagina />
-          </Route>
-          <Route path="/como-funciona">
-            <ComoFunciona />
-          </Route>
-          <Route path="/" exact>
-            <Redirect to="/inicio" />
-          </Route>
-          <Route path="*">
-            <NotFoundPagina />
-          </Route>
-        </Switch>
-      </Router>
+      <ContextoIncidenciasProvider>
+        <Router>
+          <Switch>
+            <Route path="/inicio" exact>
+              <InicioPagina />
+            </Route>
+            <Route path="/registro/:accion" exact>
+              <RegistroPagina />
+            </Route>
+            <Route path="/incidencia/:id" exact>
+              <IncidenciaPagina />
+            </Route>
+            <Route path="/nueva-incidencia" exact>
+              <NuevaIncidenciaPagina />
+            </Route>
+            <Route path="/mi-cuenta" exact>
+              <MiCuentaPagina />
+            </Route>
+            <Route path="/mis-incidencias" exact>
+              <MisIncidenciasPagina />
+            </Route>
+            <Route path="/contacto" exact>
+              <ContactoPagina />
+            </Route>
+            <Route path="/como-funciona">
+              <ComoFunciona />
+            </Route>
+            <Route path="/" exact>
+              <Redirect to="/inicio" />
+            </Route>
+            <Route path="*">
+              <NotFoundPagina />
+            </Route>
+          </Switch>
+        </Router>
+      </ContextoIncidenciasProvider>
       <Footer />
     </Container>
   );
