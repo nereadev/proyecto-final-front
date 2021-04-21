29fdd7cfe52f89ff8bb5defb3161ab261a7ad138
Malena de Juan <malena.dj4@gmail.com>
2021-04-21 13:29:28 +0200

    ternarios para mostrar menu

--- src/Componentes/MenuFooter.js
+++ src/Componentes/MenuFooter.js
@@ -10,9 +10,9 @@ const MenuFooter = () => {
       <Nav className="menuFooter">
         <Nav.Item><Nav.Link href="/como-funciona" className="navItem"> Cómo funciona?</Nav.Link></Nav.Item>
         <Nav.Item><Nav.Link href="/contacto" className="navItem">Contacto</Nav.Link></Nav.Item>
-        <Nav.Item><Nav.Link href="/registro/acceder" hidden={logeado && false} className="navItem">Acceder</Nav.Link></Nav.Item>
-        <Nav.Item><Nav.Link href="/registro/crear-cuenta" hidden={logeado && false} className="navItem">Crear cuenta</Nav.Link></Nav.Item>
-        <Nav.Item><Nav.Link hidden={!logeado && false} className="navItem">Cerrar sesión</Nav.Link></Nav.Item>
+        {!logeado && <Nav.Item><Nav.Link href="/registro/acceder" className="navItem">Acceder</Nav.Link></Nav.Item>}
+        {!logeado && <Nav.Item><Nav.Link href="/registro/crear-cuenta" className="navItem">Crear cuenta</Nav.Link></Nav.Item>}
+        {logeado && <Nav.Item><Nav.Link className="navItem">Cerrar sesión</Nav.Link></Nav.Item>}
       </Nav>
     </Col>
   );
--- src/Componentes/MenuPrincipal.js
+++ src/Componentes/MenuPrincipal.js
@@ -12,14 +12,14 @@ const MenuPrincipal = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="menuCabecera">
             <Nav.Link href="/inicio" className="navItem">Mapa / Incidencias</Nav.Link>
-            <Nav.Link href="/nueva-incidencia" hidden={!logeado && false} className="navItem">Nueva Incidencia</Nav.Link>
-            <Nav.Link href="/mis-incidencias" hidden={!logeado && false} className="navItem"> Mis incidencias</Nav.Link>
+            {logeado && <Nav.Link href="/nueva-incidencia" className="navItem">Nueva Incidencia</Nav.Link>}
+            {logeado && <Nav.Link href="/mis-incidencias" className="navItem"> Mis incidencias</Nav.Link>}
             <Nav.Link href="/como-funciona" className="navItem"> Cómo funciona?</Nav.Link>
-            <Nav.Link href="/mi-cuenta" hidden={!logeado && false} className="navItem">Mi cuenta</Nav.Link>
+            {logeado && <Nav.Link href="/mi-cuenta" className="navItem">Mi cuenta</Nav.Link>}
             <Nav.Link href="/contacto" className="navItem">Contacto</Nav.Link>
-            <Button variant="dark" onClick={(e) => { e.preventDefault(); setLogeado(true); }} hidden={logeado && false} href="/registro/acceder" className="navItem registroBoton acceder">Acceder</Button>
-            <Button variant="dark" hidden={logeado && false} href="/registro/crear-cuenta" className="navItem registroBoton crearCuenta">Crear cuenta</Button>
-            <Button variant="dark" onClick={(e) => { e.preventDefault(); setLogeado(false); }} hidden={!logeado && false} className="navItem registroBoton logoutBoton">Cerrar sesión</Button>
+            {!logeado && <Button variant="dark" onClick={(e) => { e.preventDefault(); setLogeado(true); }} href="/registro/acceder" className="navItem registroBoton acceder">Acceder</Button>}
+            {!logeado && <Button variant="dark" href="/registro/crear-cuenta" className="navItem registroBoton crearCuenta">Crear cuenta</Button>}
+            {logeado && <Button variant="dark" onClick={(e) => { e.preventDefault(); setLogeado(false); }} className="navItem registroBoton logoutBoton">Cerrar sesión</Button>}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
