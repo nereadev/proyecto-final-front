e904e906a197a55300e0accf1048bf4eb6d5f05b
Malena de Juan <malena.dj4@gmail.com>
2021-04-21 16:52:50 +0200

    reestructurado grid

--- src/paginas/ComoFunciona.js
+++ src/paginas/ComoFunciona.js
@@ -1,31 +1,31 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 
 const ComoFunciona = () => (
-  <>
-    <Row as="main">
-      <Col sm={12} as="h2">Cómo funciona?</Col>
-      <Col as="section">
-        <h3>Infórmate sobre qué incidencias hay en tu zona</h3>
-        <h3>Regístrate para votar incidencias o regístrarlas tú mismo</h3>
+  <Container as="main">
+    <Row><Col sm={12} as="h2" className="como-funciona">Cómo funciona?</Col></Row>
+    <Row>
+      <Col sm={12} as="section" className="d-flex">
+        <Row as="li"><Col as="h3" className="tip">Infórmate sobre qué incidencias hay en tu zona</Col></Row>
+        <Row as="li"><Col as="h3" className="tip">Regístrate para votar incidencias o regístrarlas tú mismo</Col></Row>
       </Col>
-      <Col as="section">
-        <h3>¿Quieres registrar una incidencia?</h3>
+      <Col sm={12} as="section">
+        <Row as="h3">¿Quieres registrar una incidencia?</Row>
         <Row as="ul" className="puntoFuncionamiento">
-          <li>Primero comprueba si alguien ya ha reportado esa incidencia. ¿Cómo?</li>
-          <ul>
-            <li>Localiza la incidencia en el mapa</li>
-            <li>Consulta el listado de incidencias utilizando filtros o el buscador para encontrar la incidencia</li>
-          </ul>
+          <Col as="li">Primero comprueba si alguien ya ha reportado esa incidencia. ¿Cómo?</Col>
+          <Row as="ul">
+            <Col as="li">Localiza la incidencia en el mapa</Col>
+            <Col as="li">Consulta el listado de incidencias utilizando filtros o el buscador para encontrar la incidencia</Col>
+          </Row>
           <Col as="li">Si la incidencia ya está registrada puedes darle tu voto para mejorar su posicionamiento</Col>
           <Col as="li">Si todavía no está registrada ve al formulario de Nueva incidencia a través del menú de navegación</Col>
-          <li>Consulta Mis incidencias para hacer el seguimiento de las incidencias que hayas registrado</li>
-          <li>
+          <Col as="li">Consulta Mis incidencias para hacer el seguimiento de las incidencias que hayas registrado</Col>
+          <Col as="li">
             Si detectas que una incidencia está duplicada puedes reportarlo en el detalle de la incidencia
-          </li>
+          </Col>
         </Row>
       </Col>
     </Row>
-  </>
+  </Container>
 );
 
 export default ComoFunciona;
