f21b7dfe151ac4a2f1231bf09dc3edb44ce1b3f5
Malena de Juan <malena.dj4@gmail.com>
2021-04-21 13:43:51 +0200

    borrado onClick menu cabecera

--- src/Componentes/MenuPrincipal.js
+++ src/Componentes/MenuPrincipal.js
@@ -17,9 +17,9 @@ const MenuPrincipal = () => {
             <Nav.Link href="/como-funciona" className="navItem"> Cómo funciona?</Nav.Link>
             {logeado && <Nav.Link href="/mi-cuenta" className="navItem">Mi cuenta</Nav.Link>}
             <Nav.Link href="/contacto" className="navItem">Contacto</Nav.Link>
-            {!logeado && <Button variant="dark" onClick={(e) => { e.preventDefault(); setLogeado(true); }} href="/registro/acceder" className="navItem registroBoton acceder">Acceder</Button>}
+            {!logeado && <Button variant="dark" href="/registro/acceder" className="navItem registroBoton acceder">Acceder</Button>}
             {!logeado && <Button variant="dark" href="/registro/crear-cuenta" className="navItem registroBoton crearCuenta">Crear cuenta</Button>}
-            {logeado && <Button variant="dark" onClick={(e) => { e.preventDefault(); setLogeado(false); }} className="navItem registroBoton logoutBoton">Cerrar sesión</Button>}
+            {logeado && <Button variant="dark" className="navItem registroBoton logoutBoton">Cerrar sesión</Button>}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
