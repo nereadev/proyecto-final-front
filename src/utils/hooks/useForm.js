3e67cbbae2db4ed119a9255d3ff362839945b325
santi07a <santi07a@gmail.com>
2021-04-22 18:26:34 +0200

    primer intento form

--- src/utils/hooks/useForm.js
+++ src/utils/hooks/useForm.js
@@ -3,6 +3,11 @@ import { useState } from "react";
 const useForm = datosFormInicial => {
   const [datosForm, setDatosForm] = useState(datosFormInicial);
   const modificarDatos = e => {
+    if (e.target.type === "file") {
+      setDatosForm({
+        [e.target.name]: e.target.files[0]
+      });
+    }
     setDatosForm({
       ...datosForm,
       [e.target.name]: e.target.value
