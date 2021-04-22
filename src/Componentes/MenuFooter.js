4389319c951387ec279bb6636dfc9ab358a9dc6b
Malena de Juan <malena.dj4@gmail.com>
2021-04-22 18:20:07 +0200

    corregido el link del footer (cerrar sesion)

--- src/Componentes/MenuFooter.js
+++ src/Componentes/MenuFooter.js
@@ -11,13 +11,13 @@ const MenuFooter = () => {
   const cerrarSesion = useSesion();
   return (
     <Col xs={12}>
-      {/* <Nav className="menuFooter">
+      <Nav className="menuFooter">
         <Nav.Item><NavLink to="/como-funciona" className="navItem"> Cómo funciona?</NavLink></Nav.Item>
         <Nav.Item><NavLink to="/contacto" className="navItem">Contacto</NavLink></Nav.Item>
         {!existeToken && <Nav.Item><NavLink to="/registro/acceder" className="navItem">Acceder</NavLink></Nav.Item>}
         {!existeToken && <Nav.Item><NavLink to="/registro/crear-cuenta" className="navItem">Crear cuenta</NavLink></Nav.Item>}
-        {existeToken && <Nav.Item><NavLink className="navItem">Cerrar sesión</NavLink></Nav.Item>}
-      </Nav> */}
+        {existeToken && <Nav.Item><NavLink to="/inicio" onClick={cerrarSesion} className="navItem">Cerrar sesión</NavLink></Nav.Item>}
+      </Nav>
     </Col>
   );
 };
