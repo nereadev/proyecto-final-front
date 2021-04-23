866f386c0d470ff0b8894e57c04d11ec30267195
Nerea García <nereagarcia.c@gmail.com>
2021-04-23 13:51:15 +0200

    filtro tipos y fecha en lista incidencias

--- src/Componentes/FiltrosIncidencias.js
+++ src/Componentes/FiltrosIncidencias.js
@@ -1,17 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import {
-  Col, Form, InputGroup, Row
+  Col, Form, InputGroup, Row, Button
 } from "react-bootstrap";
 import { ContextoIncidencias } from "../contextos/ContextoIncidencias";
 
-const FiltroIncidencias = (props) => {
+const FiltroIncidencias = () => {
   const { getIncidencias } = useContext(ContextoIncidencias);
   const setQuery = getIncidencias.setQuery;
-  const filtroMedioAmbiente = () => setQuery("medio ambiente");
-  const filtroCivismo = () => setQuery("civismo");
-  const filtroInfraestructura = () => setQuery("infraestructura");
-  const filtroOtros = () => setQuery("otros");
-
+  const setQuery2 = getIncidencias.setQuery2;
+  const [filtroTipo, setFiltroTipo] = useState(false);
+  const [filtroOrdenar, setFiltroOrdenar] = useState(false);
+  const [tipo, setTipo] = useState(false);
+  const activarTipo = (e) => {
+    setFiltroTipo(true);
+    console.log(filtroTipo);
+    setTipo(e.target.value);
+  };
+  const activarOrdenar = () => {
+    setFiltroOrdenar(!filtroOrdenar);
+  };
+  const aplicarFiltro = () => {
+    if (filtroTipo) {
+      setQuery(tipo);
+      console.log(tipo);
+    }
+    if (filtroOrdenar) {
+      setQuery2("fecha");
+    }
+  };
   return (
     <>
       <Form className="filtro-incidencias" as={Col} md={12}>
@@ -20,25 +36,18 @@ const FiltroIncidencias = (props) => {
             <Form.Label>Filtrar:</Form.Label>
             <Form.Control as="select" defaultValue="Tipo...">
               <option>Tipo...</option>
-              <option onClick={filtroMedioAmbiente}> Medio Ambiente</option>
-              <option onClick={filtroCivismo}>Civismo</option>
-              <option onClick={filtroInfraestructura}> Infraestrucutra</option>
-              <option onClick={filtroOtros}>Otros</option>
+              <option value="medio ambiente" onClick={activarTipo}> Medio Ambiente</option>
+              <option value="civismo" onClick={activarTipo}>Civismo</option>
+              <option value="infraestructura" onClick={activarTipo}> Infraestrucutra</option>
+              <option value="otros" onClick={activarTipo}>Otros</option>
             </Form.Control>
-            <InputGroup>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Código Postal..."
-              />
-            </InputGroup>
           </Form.Group>
           <Form.Group as={Col} className="filtro-ordenar-por">
             <Form.Label>Ordenar Por:</Form.Label>
             <Form.Control as="select" defaultValue="Elige...">
               <option>Elige...</option>
               <option>Relevancia</option>
-              <option>Fecha</option>
+              <option onClick={activarOrdenar}>Fecha</option>
             </Form.Control>
           </Form.Group>
           <InputGroup as={Col} className="filtro-buscar">
@@ -51,6 +60,9 @@ const FiltroIncidencias = (props) => {
               placeholder="Buscar..."
             />
           </InputGroup>
+        </Row>
+        <Row>
+          <Button onClick={aplicarFiltro}>Aplicar</Button>
         </Row>
       </Form>
     </>
