d69bda0510f56b50ada14865fc0ef61938b1abfa
santi07a <santi07a@gmail.com>
2021-04-24 13:14:08 +0200

    methd delete funcionando, falta fx de boton

--- src/paginas/MisIncidenciasPagina.js
+++ src/paginas/MisIncidenciasPagina.js
@@ -1,17 +1,36 @@
 // eslint-disable-next-line camelcase
 import jwt_decode from "jwt-decode";
 import { useContext, useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import {
+  Col, Row, Button, Toast
+} from "react-bootstrap";
+import { useHistory } from "react-router";
 import Listado from "../componentes/Listado";
 import { ContextoIncidencias } from "../contextos/ContextoIncidencias";
+import useFetch from "../utils/hooks/useFetch";
 
 const MisIncidenciasPagina = () => {
   const token = localStorage.getItem("token-usuario");
   const idUsuario = jwt_decode(token).id;
+  const history = useHistory();
   const imgPopup = idIncidencia => (`https://firebasestorage.googleapis.com/v0/b/proyecto-final-c019d.appspot.com/o/${idIncidencia}?alt=media`);
   const getIconCircular = (tipoIncidencia) => `/img/${tipoIncidencia.split(" ").join("-")}-circular.png`;
   const { getIncidencias } = useContext(ContextoIncidencias);
   const incidencias = getIncidencias.incidencias;
+  const [ventana, setVentana] = useState(false);
+  const toggleVentana = () => setVentana(!ventana);
+  const { datos, pideDatos: deleteDatos } = useFetch();
+  const linkInicio = () => {
+    history.push("/inicio");
+  };
+  const eliminaIncidencia = (idIncidencia) => {
+    deleteDatos(false, (`http://localhost:5000/incidencias/${idIncidencia}`), {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  };
 
   return (
     <Col>
@@ -19,7 +38,7 @@ const MisIncidenciasPagina = () => {
         incidencias.length !== 0 && (
           incidencias.body.incidencias.filter(incidencia => (incidencia.usuarioCreador ? incidencia.usuarioCreador._id === idUsuario : false)).map(incidencia => (
             <>
-              <Row key={incidencia._id} className="targeta-incidencia">
+              <Row key={incidencia._id} className={`targeta-incidencia formulario ${!ventana ? "" : "oculto"} p-3`}>
                 <Col sm={2}>
                   <Row className="elemento-targeta-incidencia lateral-targeta-incidencia">{incidencia.votos}</Row>
                   <Row className="elemento-targeta-incidencia lateral-targeta-incidencia">
@@ -46,7 +65,23 @@ const MisIncidenciasPagina = () => {
                 </Col>
                 <Col sm={3} as="img" className="elemento-targeta-incidencia" src={imgPopup(incidencia.fotoIncidencia)} alt=" " />
                 <Col><a href="./incidencia/:id"><i className="fas fa-plus" aria-label="Detalle incidencia" /></a></Col>
+                <Button className="boton-nueva btn-danger" type="button" variant="info" onClick={toggleVentana}>Eliminar incidencia</Button>
               </Row>
+              <Col className="ventana" sm={12}>
+                <Toast show={ventana} onClose={linkInicio}>
+                  <Toast.Header>
+                    <i className="fas fa-check-circle mr-2" />
+                    <strong className="mr-auto">¡Cuidado!</strong>
+                    <small>cerrar</small>
+                  </Toast.Header>
+                  <Toast.Body>¿Está seguro que quiere eliminar esta incidencia?</Toast.Body>
+                  <Col>
+                    {" "}
+                    {console.log(incidencia)}
+                    <Button className="boton-nueva btn-danger btn-sm offset-4" onClick={() => eliminaIncidencia(incidencia._id)} type="button" variant="info">Eliminar</Button>
+                  </Col>
+                </Toast>
+              </Col>
             </>
           )))
       }
