5ced22ebbec6368c9a83389b4f1aee1c00103725
santi07a <santi07a@gmail.com>
2021-04-24 17:18:51 +0200

    bugfix mi cuenta

--- src/paginas/MiCuentaPagina.js
+++ src/paginas/MiCuentaPagina.js
@@ -1,4 +1,6 @@
-import { Button, Col, Row } from "react-bootstrap";
+import {
+  Button, Col, Container, Row
+} from "react-bootstrap";
 import { useContext, useEffect } from "react";
 import { ContextoUsuario } from "../contextos/ContextoUsuario";
 
@@ -9,36 +11,38 @@ const MiCuentaPagina = () => {
     console.log(usuario);
   }, [usuario]);
   return (
-    <Row as="main">
+    <Container as="main">
       <Row as="h2">Mi cuenta</Row>
-      <Col className="mi-cuenta" as="ul">
-        <Row>
-          <Col>Nombre:</Col>
-          <Col>{(usuario.length !== 0) ? usuario.body.usuario.nombre : ""}</Col>
-          <Col />
-        </Row>
-        <Row>
-          <Col>Apellidos:</Col>
-          <Col>{(usuario.length !== 0) ? usuario.body.usuario.apellidos : ""}</Col>
-          <Col />
-        </Row>
-        <Row>
-          <Col>Email:</Col>
-          <Col>{(usuario.length !== 0) ? usuario.body.usuario.email : ""}</Col>
-          <Button as={Col} className="modificar-cuenta" type="submit" variant="light">Modificar</Button>
-        </Row>
-        <Row>
-          <Col>Contraseña:</Col>
-          <Col>***********</Col>
-          <Button as={Col} className="modificar-cuenta" type="submit" variant="light">Modificar</Button>
-        </Row>
-        <Row>
-          <Col>Mis incidencias</Col>
-          <Col><a href="./mis-incidencias"><i className="fas fa-plus" aria-label="Mis incidencias" /></a></Col>
-          <Col />
-        </Row>
-      </Col>
-    </Row>
+      <Row>
+        <Col className="mi-cuenta" as="ul">
+          <Row>
+            <Col>Nombre:</Col>
+            <Col>{(usuario.length !== 0) ? usuario.body.usuario.nombre : ""}</Col>
+            <Col />
+          </Row>
+          <Row>
+            <Col>Apellidos:</Col>
+            <Col>{(usuario.length !== 0) ? usuario.body.usuario.apellidos : ""}</Col>
+            <Col />
+          </Row>
+          <Row>
+            <Col>Email:</Col>
+            <Col>{(usuario.length !== 0) ? usuario.body.usuario.email : ""}</Col>
+            <Button as={Col} className="modificar-cuenta" type="submit" variant="light">Modificar</Button>
+          </Row>
+          <Row>
+            <Col>Contraseña:</Col>
+            <Col>***********</Col>
+            <Button as={Col} className="modificar-cuenta" type="submit" variant="light">Modificar</Button>
+          </Row>
+          <Row>
+            <Col>Mis incidencias</Col>
+            <Col><a href="./mis-incidencias"><i className="fas fa-plus" aria-label="Mis incidencias" /></a></Col>
+            <Col />
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
