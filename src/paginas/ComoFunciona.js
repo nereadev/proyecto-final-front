feb69812f7255a4b85444a56afa7be8a60565cbf
Malena de Juan <malena.dj4@gmail.com>
2021-04-24 09:05:03 +0200

    cambiado al tipo de disposicion antigua

--- src/paginas/ComoFunciona.js
+++ src/paginas/ComoFunciona.js
@@ -4,30 +4,28 @@ import {
 
 const ComoFunciona = () => (
   <Row as="main">
-    <Col sm={12} as="h2" className="como-funciona text-center">Cómo funciona?</Col>
-    {/* <Row>
-      <Col sm={12} as="section" className="d-flex">
-        <Row as="li"><Col as="h3" className="tip">Infórmate sobre qué incidencias hay en tu zona</Col></Row>
-        <Row as="li"><Col as="h3" className="tip">Regístrate para votar incidencias o regístrarlas tú mismo</Col></Row>
-      </Col>
-      <Col sm={12} as="section">
-        <Row as="h3">¿Quieres registrar una incidencia?</Row>
-        <Row as="ul" className="puntoFuncionamiento">
-          <Col as="li">Primero comprueba si alguien ya ha reportado esa incidencia. ¿Cómo?</Col>
-          <Row as="ul">
-            <Col as="li">Localiza la incidencia en el mapa</Col>
-            <Col as="li">Consulta el listado de incidencias utilizando filtros o el buscador para encontrar la incidencia</Col>
-          </Row>
-          <Col as="li">Si la incidencia ya está registrada puedes darle tu voto para mejorar su posicionamiento</Col>
-          <Col as="li">Si todavía no está registrada ve al formulario de Nueva incidencia a través del menú de navegación</Col>
-          <Col as="li">Consulta Mis incidencias para hacer el seguimiento de las incidencias que hayas registrado</Col>
-          <Col as="li">
-            Si detectas que una incidencia está duplicada puedes reportarlo en el detalle de la incidencia
-          </Col>
-        </Row>
-      </Col>
-    </Row> */}
-    <Col>
+    <Col sm={12} as="h2" className="text-center">Cómo funciona?</Col>
+    <Col sm={12} as="section" className="">
+      <ul className="list-unstyled text-center d-flex">
+        <li clasaName="tips-titulos"><h3>Infórmate sobre qué incidencias hay en tu zona</h3></li>
+        <li clasaName="tips-titulos"><h3>Regístrate para votar incidencias o regístrarlas tú mismo</h3></li>
+      </ul>
+      <h3>¿Quieres registrar una incidencia?</h3>
+      <ul>
+        <li className="tips">Primero comprueba si alguien ya ha reportado esa incidencia. ¿Cómo?</li>
+        <ul className="mini-lista">
+          <li className="mini-tips">Localiza la incidencia en el mapa</li>
+          <li className="mini-tips">Consulta el listado de incidencias utilizando filtros o el buscador para encontrar la incidencia</li>
+        </ul>
+        <li className="tips">Si la incidencia ya está registrada puedes darle tu voto para mejorar su posicionamiento</li>
+        <li className="tips">Si todavía no está registrada ve al formulario de Nueva incidencia a través del menú de navegación</li>
+        <li className="tips">Consulta Mis incidencias para hacer el seguimiento de las incidencias que hayas registrado</li>
+        <li className="tips">
+          Si detectas que una incidencia está duplicada puedes reportarlo en el detalle de la incidencia
+        </li>
+      </ul>
+    </Col>
+    {/*  <Col>
       <Row as="ul" className="lista-tips list-unstyled">
         <Col sm={4} as="li" className="tips-titulos"><span>Infórmate sobre qué incidencias hay en tu zona</span></Col>
         <Col sm={4} as="li" className="tips-titulos"><span>Regístrate para votar incidencias o regístrarlas tú mismo</span></Col>
@@ -40,7 +38,7 @@ const ComoFunciona = () => (
         <Col sm={12} as="li"><span>3. Consulta Mis incidencias para hacer el seguimiento de las incidencias que hayas registrado</span></Col>
         <Col sm={12} as="li"><span>Si detectas que una incidencia está duplicada puedes reportarlo en el detalle de la incidencia</span></Col>
       </Row>
-    </Col>
+    </Col> */}
   </Row>
 );
 
