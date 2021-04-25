4208d06f95cc2cd73f1a204ba9a1912ee27684ab
Bernat Jorquera <bernatjv19@gmail.com>
2021-04-25 14:44:39 +0200

    Mapa por distritos mas cerca de como deberia ser

--- src/Componentes/Mapa.js
+++ src/Componentes/Mapa.js
@@ -26,16 +26,16 @@ https://github.com/martgnz/bcn-geodata */
 const token = "pk.eyJ1IjoiYmVybmF0anYiLCJhIjoiY2tub2o2emxzMWVweTJxbnhicGxiejRvOCJ9.x-GGbqA5iOhR66FnJ4DWnw";
 const urlMapbox = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${token}`;
 const urlMapboxEUA = `https://api.mapbox.com/styles/v1/bernatjv/cknouqecv5hu017s5twegcpys/tiles/256/{z}/{x}/{y}@2x?access_token=${token}`;
-const urlMapboxBarris = `https://api.mapbox.com/styles/v1/bernatjv/cknow7tl91o6l17o1awn9zgcf/tiles/256/{z}/{x}/{y}@2x?access_token=${token}`;
+const urlMapboxDistritos = `https://api.mapbox.com/styles/v1/bernatjv/cknow7tl91o6l17o1awn9zgcf/tiles/256/{z}/{x}/{y}@2x?access_token=${token}`;
 const urlDefault = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-const urlBarrios = `https://api.mapbox.com/geocoding/v5/mapbox.places/2.167612130848904,41.38993034972496.json?access_token=${token}`;
+const urlDistritos = `https://api.mapbox.com/geocoding/v5/mapbox.places/2.167612130848904,41.38993034972496.json?access_token=${token}`;
 const coordsBCN = [41.38993034972496, 2.167612130848904];
 const coordsEUA = [39.58943721614868, -100.591495305186];
 const imgPopup = idIncidencia => (`https://firebasestorage.googleapis.com/v0/b/proyecto-final-c019d.appspot.com/o/${idIncidencia}?alt=media`);
 
 // para localizar barrio/distrito de cada incidencia
 const fetchBarrio = () => {
-  fetch(urlBarrios).then(resp => resp.json()).then(datos => console.log(datos));
+  fetch(urlDistritos).then(resp => resp.json()).then(datos => console.log(datos));
 };
 
 const getIcon = (tipoIncidencia) => icon({
@@ -52,58 +52,60 @@ const Mapa = () => {
   const filtrarTipo = tipo => {
     setQuery(tipo);
   };
-  const [mapaBarrios, setMapaBarrios] = useState(false);
+  const [mapaDistritos, setMapaDistritos] = useState(false);
   return (
     <>
       <Nav fill defaultActiveKey="incidencias">
         <Nav.Item>
-          <NavLink eventKey="incidencias" className="tiposMapa" onSelect={() => setMapaBarrios(false)}>Puntos de incidencia</NavLink>
+          <NavLink eventKey="incidencias" className="tiposMapa" onSelect={() => setMapaDistritos(false)}>Puntos de incidencia</NavLink>
         </Nav.Item>
         <Nav.Item>
-          <NavLink eventKey="barrios" className="tiposMapa" onSelect={() => setMapaBarrios(true)}>Incidencias por barrios</NavLink>
+          <NavLink eventKey="distritos" className="tiposMapa" onSelect={() => setMapaDistritos(true)}>Incidencias por distritos</NavLink>
         </Nav.Item>
       </Nav>
-      <Nav fill className="filtros-mapa">
-        <Nav.Item>
-          <NavLink onClick={() => filtrarTipo("civismo")} eventKey="civismo" className="filtro-iconos">
-            <img src="img/civismo-circular.png" alt="icono civismo" />
-            Civismo
-          </NavLink>
-        </Nav.Item>
-        <Nav.Item>
-          <NavLink onClick={() => filtrarTipo("medio ambiente")} eventKey="medio-ambiente" className="filtro-iconos">
-            <img src="img/medio-ambiente-circular.png" alt="icono civismo" />
-            Medio Ambiente
-          </NavLink>
-        </Nav.Item>
-        <Nav.Item>
-          <NavLink onClick={() => filtrarTipo("infraestructura")} eventKey="infraestructura" className="filtro-iconos">
-            <img src="img/infraestructura-circular.png" alt="icono civismo" />
-            Infraestructura
-          </NavLink>
-        </Nav.Item>
-        <Nav.Item>
-          <NavLink onClick={() => filtrarTipo("otros")} eventKey="otros" className="filtro-iconos">
-            <img src="img/otros-circular.png" alt="icono civismo" />
-            Otros
-          </NavLink>
-        </Nav.Item>
-      </Nav>
+      {!mapaDistritos && (
+        <Nav fill className="filtros-mapa">
+          <Nav.Item>
+            <NavLink onClick={() => filtrarTipo("civismo")} eventKey="civismo" className="filtro-iconos">
+              <img src="img/civismo-circular.png" alt="icono civismo" />
+              Civismo
+            </NavLink>
+          </Nav.Item>
+          <Nav.Item>
+            <NavLink onClick={() => filtrarTipo("medio ambiente")} eventKey="medio-ambiente" className="filtro-iconos">
+              <img src="img/medio-ambiente-circular.png" alt="icono civismo" />
+              Medio Ambiente
+            </NavLink>
+          </Nav.Item>
+          <Nav.Item>
+            <NavLink onClick={() => filtrarTipo("infraestructura")} eventKey="infraestructura" className="filtro-iconos">
+              <img src="img/infraestructura-circular.png" alt="icono civismo" />
+              Infraestructura
+            </NavLink>
+          </Nav.Item>
+          <Nav.Item>
+            <NavLink onClick={() => filtrarTipo("otros")} eventKey="otros" className="filtro-iconos">
+              <img src="img/otros-circular.png" alt="icono civismo" />
+              Otros
+            </NavLink>
+          </Nav.Item>
+        </Nav>
+      )}
       <MapContainer center={coordsBCN} zoom={13} scrollWheelZoom={false} className="mapa">
-        {!mapaBarrios || (
+        {!mapaDistritos || (
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url={urlMapboxBarris}
+            url={urlMapboxDistritos}
           />
         )}
-        {mapaBarrios || (
+        {mapaDistritos || (
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url={urlMapbox}
           />
         )}
         {
-          incidencias.length !== 0 && (
+          (incidencias.length !== 0 && !mapaDistritos) && (
             incidencias.body.incidencias.map(incidencia => (
               !incidencia.latitud || (
                 <Marker key={incidencia._id} position={[incidencia.latitud, incidencia.longitud]} icon={getIcon(incidencia.tipoIncidencia.tipo)}>
