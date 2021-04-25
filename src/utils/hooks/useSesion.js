c7ed16cf6ed3520c3b126c14f63dbedb63aa39f3
Malena de Juan <malena.dj4@gmail.com>
2021-04-25 14:22:46 +0200

    al cerrar sesion volver a inicio

--- src/utils/hooks/useSesion.js
+++ src/utils/hooks/useSesion.js
@@ -1,11 +1,14 @@
 import { useContext } from "react";
+import { useHistory } from "react-router";
 import { ContextoToken } from "../../contextos/ContextoToken";
 
 const useSesion = () => {
   const { setExisteToken } = useContext(ContextoToken);
+  const history = useHistory();
   const cerrarSesion = () => {
     localStorage.removeItem("token-usuario");
     setExisteToken(false);
+    history.push("/inicio");
   };
   return cerrarSesion;
 };
